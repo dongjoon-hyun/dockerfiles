241dfe87c8521047c67793bc79c721f74932b9fa
Dongjoon Hyun <dongjoon@apache.org>
2014-11-17 01:47:48 -0500

    add cluster building scripts

---
+++ ubuntu14.10-hdw/run-cluster.sh
@@ -0,0 +1,16 @@
+LINK=""
+for i in {1..3}
+do
+    HOST=hdw-001-0$i
+    LINK="$LINK --link=$HOST:$HOST"
+    docker run --name=$HOST -h $HOST -p 1000$i:22 -p 1920$i:9200 -d sktelecom/ubuntu14.10-hdw /root/start.sh
+done
+
+HOST=hnn-001-01
+PORT="-p 10000:22 -p 50070:50070 -p 8088:8088"
+docker run --name=$HOST -h $HOST $PORT $LINK -it --rm sktelecom/ubuntu14.10-hdw /root/init-nn.sh
+
+for i in {1..3}
+do
+    docker rm -f hdw-001-0$i
+done
