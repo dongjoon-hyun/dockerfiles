cc22928b1283f8d64317a784987a80c7a36b4595
Dongjoon Hyun <dongjoon@apache.org>
2015-03-16 06:24:11 +0900

    bug fix

--- centos6-hdw/test-spark.sh
+++ centos6-hdw/test-spark.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-/usr/local/spark/bin/spark-submit --class org.apache.spark.examples.SparkPi --master yarn-cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 $SPARK_HOME/lib/spark-examples-1.2.1-hadoop2.4.0.jar
+/usr/local/spark/bin/spark-submit --class org.apache.spark.examples.SparkPi --master yarn-cluster --driver-memory 1g --executor-memory 1g --executor-cores 1 $SPARK_HOME/lib/spark-examples-*-hadoop2.4.0.jar
