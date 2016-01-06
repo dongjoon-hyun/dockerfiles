fc4ee53fe19afecda6f968e0ebc886483823c76f
Dongjoon Hyun <dongjoon@apache.org>
2016-01-06 00:18:18 -0800

    Fix missing postfix '-hdw'

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 docker build -t dongjoon/centos7-hdw centos7-hdw
 docker build -t dongjoon/ubuntu15.10-hdw ubuntu15.10-hdw
 docker build -t dongjoon/debian8-hdw debian8-hdw
-docker build -t dongjoon/java7-hdw java7
-docker build -t dongjoon/java8-hdw java8
+docker build -t dongjoon/java7-hdw java7-hdw
+docker build -t dongjoon/java8-hdw java8-hdw
