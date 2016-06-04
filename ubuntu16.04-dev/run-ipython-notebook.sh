15bc9154dc2a4586c24d206fbbb6dfe91df9bb99
Dongjoon Hyun <dongjoon@apache.org>
2016-06-04 16:26:24 -0700

    Replace `yarn-client` with `yarn`

--- ubuntu16.04-dev/run-ipython-notebook.sh
+++ ubuntu16.04-dev/run-ipython-notebook.sh
@@ -1,1 +1,1 @@
-PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --master yarn-client &>ipython.log &
+PYSPARK_DRIVER_PYTHON=ipython PYSPARK_DRIVER_PYTHON_OPTS="notebook --no-browser" pyspark --master yarn &>ipython.log &
