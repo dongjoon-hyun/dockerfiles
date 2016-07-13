b28fb9b0d480e0288247655eb3a34d74937678a5
Dongjoon Hyun <dongjoon@apache.org>
2016-07-12 21:10:40 -0700

    Update build.sh by adding dev dockers. (#32)

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@ docker build -t dongjoon/centos7-hdw centos7-hdw
 docker build -t dongjoon/ubuntu12.04-hdw ubuntu12.04-hdw
 docker build -t dongjoon/ubuntu15.10-hdw ubuntu15.10-hdw
 docker build -t dongjoon/ubuntu16.04-hdw ubuntu16.04-hdw
+docker build -t dongjoon/ubuntu16.04-dev ubuntu16.04-dev
 docker build -t dongjoon/debian8-hdw debian8-hdw
 docker build -t dongjoon/java7-hdw java7-hdw
 docker build -t dongjoon/java8-hdw java8-hdw
+docker build -t dongjoon/java9-test java9-test
