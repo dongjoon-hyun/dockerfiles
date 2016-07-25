c2feee0b76d778567eb47cbc2eb08224cca11cee
Dongjoon Hyun <dongjoon@apache.org>
2016-07-24 19:13:44 -0700

    Remove Ubuntu15.10

--- build.sh
+++ build.sh
@@ -1,6 +1,5 @@
 docker build -t dongjoon/centos7-hdw centos7-hdw
 docker build -t dongjoon/ubuntu12.04-hdw ubuntu12.04-hdw
-docker build -t dongjoon/ubuntu15.10-hdw ubuntu15.10-hdw
 docker build -t dongjoon/ubuntu16.04-hdw ubuntu16.04-hdw
 docker build -t dongjoon/ubuntu16.04-dev ubuntu16.04-dev
 docker build -t dongjoon/debian8-hdw debian8-hdw
