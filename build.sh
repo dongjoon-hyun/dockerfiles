66e4e51ddaef0a83e496e36e41fe393baea1e5fd
Dongjoon Hyun <dongjoon@apache.org>
2016-07-30 05:11:21 -0700

    Simplify build.sh.

--- build.sh
+++ build.sh
@@ -1,8 +1,5 @@
-docker build -t dongjoon/centos7-hdw centos7-hdw
-docker build -t dongjoon/ubuntu12.04-hdw ubuntu12.04-hdw
-docker build -t dongjoon/ubuntu16.04-hdw ubuntu16.04-hdw
-docker build -t dongjoon/ubuntu16.04-dev ubuntu16.04-dev
-docker build -t dongjoon/debian8-hdw debian8-hdw
-docker build -t dongjoon/java7-hdw java7-hdw
-docker build -t dongjoon/java8-hdw java8-hdw
-docker build -t dongjoon/java9-test java9-test
+ORG=dongjoon
+for DIR in `ls -d */`
+do
+    docker build -t $ORG/${DIR%%/} ${DIR%%/}
+done
